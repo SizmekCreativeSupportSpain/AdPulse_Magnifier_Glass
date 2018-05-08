464775df2cfb736c3daf7f33085bb1c21fbbecda
Javier Egido <javier.egido.alonso@gmail.com>
2018-05-08 21:24:14 +0200

    Initial commit

---
+++ Template Files/panels/expand/script.js
@@ -0,0 +1,83 @@
+/*******************
+VARIABLES
+*******************/
+var lastModified = "2017-08-02";
+var lastUploaded = "2017-08-02";
+var templateVersion = "2.0.24";
+
+var expansionDiv;
+var closeButton;
+var adId, rnd, uid;
+var imageContainer;
+
+/*******************
+INITIALIZATION
+*******************/
+function checkIfAdKitReady(event) {
+	adkit.onReady(initializeCreative);
+	expansionDiv = document.getElementById("expansion");
+	closeButton = document.getElementById("closeButton");
+	imageContainer = document.getElementById('imageContainer');
+
+	closeButton.addEventListener("click", handleCloseButtonClick);
+	imageContainer.addEventListener('mouseenter', onMouseEnter);
+	imageContainer.addEventListener('mouseleave', onMouseLeave);
+}
+
+function initializeCreative(event) {
+	try { //try/catch just in case localPreview.js is not included
+		if (window.localPreview) {
+			window.initializeLocalPreview(); //in localPreview.js
+		}
+	}
+	catch (e) {}
+
+	//Workaround (from QB6573) for Async EB Load where Modernizr isn't properly initialized
+	typeof Modernizr === "object" && (Modernizr.touch = Modernizr.touch || "ontouchstart" in window);
+
+	window.registerInteraction = function() {}; //overwrite rI function because it will never actually be called
+	initializeGlobalVariables();
+}
+
+function initializeGlobalVariables () {
+	adId = EB._adConfig.adId;
+	rnd = EB._adConfig.rnd;
+	uid = EB._adConfig.uid;
+}
+
+function handleCloseButtonClick(e){
+	adkit.collapse({
+     panelName: "expand",
+     actionType: adkit.ActionType.USER,
+     animate: {
+        clip: "rect(0, 100%, 0, 100%)",
+        duration: 300,
+        easing: adkit.Animation.Easing.EASE_IN,
+        opacity: 0,
+        init: {
+           clip: "rect(0, 100%, 100%, 0)",
+           opacity: 1
+        }
+     }
+   });
+}
+
+window.addEventListener("load", checkIfAdKitReady);
+
+
+function onMouseEnter(event){
+	imageContainer.removeEventListener('mouseenter', onMouseEnter);
+	imageContainer.addEventListener('mousemove', onMouseMove);
+	imageContainer.addEventListener('click', function(){
+		imageContainer.style.display = 'none';
+		document.getElementById('imageContainerExp').style.opacity = '1';
+	});
+}
+function onMouseMove(event){
+	event.target.style.webkitMaskImage = 'radial-gradient(circle 100px at ' + event.pageX + 'px ' + event.pageY + 'px, rgba(255,255,255,1) 80%, rgba(255,255,255,0) 100%)';
+	event.target.style.cursor = 'none';
+}
+function onMouseLeave(event){
+	imageContainer.addEventListener('mouseenter',onMouseEnter);
+	imageContainer.removeEventListener('mousemove', onMouseMove);
+}
